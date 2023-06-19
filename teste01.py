a80b88b4e568db8569cefb285962678df0b6bbb4
mayarakaren <mayarakaren5910@gmail.com>
2023-06-19 18:09:34 -0300

    Fix: Inicial

---
+++ teste01.py
@@ -0,0 +1,24 @@
+num = -3
+num_games = 14
+num_conv = 15
+
+print(num, num_games) 
+
+pi = 3.14
+terremoto = -4.55
+euler = 2.71
+
+print(terremoto, pi) 
+
+# letra = 'A'
+# word1 = 'linguagem de programação'
+# word2 = 'Python'
+
+""" print(letra, word1) """
+
+""" print('Estou aprendendo uma', word1) """
+
+# print('Esta', word1, 'se chama', word2)
+
+age = int(input('Digite sua idade:'))
+print('Sua idade é:', age)
